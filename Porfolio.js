687cbfd87579f7de3ba9e0a2b31de55a39723cf4
Morgan0706 <morganmajwe06@gmail.com>
2025-04-01 22:07:58 +0200

    Update Porfolio.js

--- Porfolio.js
+++ Porfolio.js
@@ -1,7 +1,1 @@
-// JavaScript for Sidebar Toggle
-const toggleBtn = document.getElementById("toggle-btn");
-const sidebar = document.querySelector(".sidebar");
 
-toggleBtn.addEventListener("click", function() {
-    sidebar.classList.toggle("open");
-});
